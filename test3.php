b26f50bce4ed53467301c575273b449d71355226
Franco Benedetti <franco@growthhq.net>
2026-08-04 12:12:30 +0200

    chore: remove temporary test scripts and log file

--- test3.php
+++
@@ -1,5 +0,0 @@
-<?php
-$rawInput = '{"tree": [{"id": "b3", "title": "Book 3"}, {"id": "b1", "title": "Book 1"}, {"id": "b2", "title": "Book 2"}]}';
-$json = json_decode($rawInput, true);
-$tree = $json['tree'] ?? $_POST['tree'] ?? null;
-var_dump($tree);
--- test4.php
+++
@@ -1,8 +0,0 @@
-<?php
-$configFile = __DIR__ . '/qwiki.json';
-$config = json_decode(file_get_contents($configFile), true);
-$tree = $config['books'];
-$tree = array_reverse($tree); // Swap order for testing
-$config['books'] = $tree;
-file_put_contents($configFile, json_encode($config, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
-echo "Modified qwiki.json\n";
